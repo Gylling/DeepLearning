9e4c2ccf9a8724e50615521a18559943d89b7696
Erik Gylling <gylling.eriuk@gmail.com>
2021-11-22 14:54:53 +0100

    no hack

--- PPO.py
+++ PPO.py
@@ -122,7 +122,7 @@ def create_and_train_network():
     # Define environment
     # check the utils.py file for info on arguments
     count = 0
-    env = make_env(num_envs, env_name=choose_game(count), num_levels=1)
+    env = make_env(num_envs, env_name=choose_game(count), num_levels=num_levels)
     print('Observation space:', env.observation_space.shape)
     print('Action space:', env.action_space.n)
     channels_in = env.observation_space.shape[0]
@@ -212,9 +212,9 @@ def create_and_train_network():
         # Update stats
         step += num_envs * num_steps
         print(f'Step: {step}\tMean reward: {storage.get_reward()}')
-        count = (count + 1) % num_levels
-        env = make_env(num_envs, env_name=choose_game(count), start_level=count, num_levels=1)
-        obs = env.reset()
+        # count = (count + 1) % num_levels
+        # env = make_env(num_envs, env_name=choose_game(count), start_level=count, num_levels=1)
+        # obs = env.reset()
 
     print('Completed training!')
     torch.save(policy.state_dict, f'checkpoints/{FOLDER_NAME}/checkpoint-{time.time()}.pt')
