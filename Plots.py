957b043e9ade9958453e30900deef528d7a428f8
Erik Gylling <gylling.eriuk@gmail.com>
2021-12-07 15:22:58 +0100

    Plot is right now

--- Plots.py
+++ Plots.py
@@ -25,46 +25,42 @@ file_dict = {
     },
     "plr-td": {
         "starpilot": {"test": "rewards/plr-starpilot-OneStep/test_rewards-1638828612.513363.csv", "train": "rewards/plr-starpilot-OneStep/train_rewards-1638828612.5180354.csv"},
-        "heist": {"test": "rand2.csv", "train": "rand.csv"},
+        "heist": {"test": "", "train": ""},
         "maze": {"test": "rewards/plr-maze-OneStep/test_rewards-1638809491.8857126.csv", "train": "rewards/plr-maze-OneStep/train_rewards-1638809491.8898652.csv"}
     },
     "plr-gae": {
-        "starpilot": {"test": "rrewards/plr-starpilot-GAE/test_rewards-1638867795.0073612.csv", "train": "rewards/plr-starpilot-GAE/train_rewards-1638867795.0123215.csv"},
-        "heist": {"test": "rand3.csv", "train": "rand.csv"},
-        "maze": {"test": "rand3.csv", "train": "rand.csv"}
+        "starpilot": {"test": "rewards/plr-starpilot-GAE/test_rewards-1638867795.0073612.csv", "train": "rewards/plr-starpilot-GAE/train_rewards-1638867795.0123215.csv"},
+        "heist": {"test": "", "train": ""},
+        "maze": {"test": "", "train": ""}
     },
     "plr-gaem": {
         "starpilot": {"test": "rewards/plr-starpilot-GAEMag/test_rewards-1638825768.9538002.csv", "train": "rewards/plr-starpilot-GAEMag/train_rewards-1638825768.960656.csv"},
-        "heist": {"test": "rand4.csv", "train": "rand.csv"},
+        "heist": {"test": "", "train": ""},
         "maze": {"test": "rewards/plr-maze-GAEMag/test_rewards-1638815208.0819683.csv", "train": "rewards/plr-maze-GAEMag/train_rewards-1638815208.0867116.csv"}
     }
 }
 
-x = np.linspace(0, 25, 25)
+x = np.linspace(1, 22, 22)
 
 
 def generate_graph(fig, colors, ax, ax_coord, list_of_paths, label_names, title="Results", xlabel="Steps in M",
                    ylabel="Rewards"):
     ax_x, ax_y = ax_coord
-    test = False
     # 2: Loop through paths
     for path, label, color in zip(list_of_paths, label_names, colors):
-        if test:
-            df = np.random.sample(25, )
+        if(len(path) == 0):
+            continue
         else:
             df = pd.read_csv(path, header=None)
-
         ax[ax_x, ax_y].plot(x, df, label=label, c=color)
 
-        if not (ax_x == 0 and ax_y == 0):
-            ax[ax_x, ax_y].set_xlabel(xlabel)
-        if ax_y == 0:
-            ax[ax_x, ax_y].set_ylabel(ylabel)
+        ax[ax_x, ax_y].set_xlabel(xlabel)
+        ax[ax_x, ax_y].set_ylabel(ylabel)
 
         ax[ax_x, ax_y].set_title(title)
 
 
-fig, axs = plt.subplots(2, 2, sharey=True)
+fig, axs = plt.subplots(2, 2, sharey=False)
 # 1: Initialize variables
 # DO THIS 4 times, one for each subplot
 
@@ -94,9 +90,11 @@ for game, coord in zip(games, coords):
 fig.delaxes(axs[1][1])
 
 # handles, labels = axs[1, 1].get_legend_handles_labels()
-axs[0, 1].legend(bbox_to_anchor=(1.05, 0), loc='lower left', borderaxespad=0.)
+# axs[0, 0].legend(bbox_to_anchor=(1.05, 0), loc='lower left', borderaxespad=0.)
 # fig.legend(handles, labels, loc=(0.5, 0), ncol=3)
 
+fig.tight_layout()
+plt.savefig('rewards.png')
 fig.show()
 
 """
