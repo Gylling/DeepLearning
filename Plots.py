27097de7d40ca8e79d378d663cf333b3effd3693
Christian Bøgelund <s171243@student.dtu.dk>
2021-12-28 12:20:03 +0100

    Plots works

--- Plots.py
+++ Plots.py
@@ -19,28 +19,28 @@ Plot 1..n:
 
 file_dict = {
     "ppo": {
-        "junper": {"test": "rewards/ppo-jumper/test_rewards-1640046985.3327672.csv", "train": "rewards/ppo-jumper/train_rewards-1640046985.3378258.csv"},
+        "jumper": {"test": "rewards/ppo-jumper/test_rewards-1640046985.3327672.csv", "train": "rewards/ppo-jumper/train_rewards-1640046985.3378258.csv"},
         "ninja": {"test": "rewards/ppo-ninja/test_rewards-1640011771.8472745.csv", "train": "rewards/ppo-ninja/train_rewards-1640011771.8518178.csv"},
         "bigfish": {"test": "rewards/ppo-bigfish/test_rewards-1639986849.5726612.csv", "train": "rewards/ppo-bigfish/train_rewards-1639986849.5768883.csv"}
     },
     "plr-td": {
-        "junper": {"test": "rewards/plr-jumper-OneStep/test_rewards-1640123833.4178863.csv", "train": "rewards/plr-jumper-OneStep/train_rewards-1640123833.4224465.csv"},
+        "jumper": {"test": "rewards/plr-jumper-OneStep/test_rewards-1640123833.4178863.csv", "train": "rewards/plr-jumper-OneStep/train_rewards-1640123833.4224465.csv"},
         "ninja": {"test": "rewards/plr-ninja-OneStep/test_rewards-1640145094.9311488.csv", "train": "rewards/plr-ninja-OneStep/train_rewards-1640145094.9371839.csv"},
         "bigfish": {"test": "rewards/plr-bigfish-OneStep/test_rewards-1640061956.0442092.csv", "train": "rewards/plr-bigfish-OneStep/train_rewards-1640061956.04891.csv"}
     },
     "plr-gae": {
-        "junper": {"test": "rewards/plr-jumper-GAE/test_rewards-1640360949.4017658.csv", "train": "rewards/plr-jumper-GAE/train_rewards-1640360949.407864.csv"},
+        "jumper": {"test": "rewards/plr-jumper-GAE/test_rewards-1640360949.4017658.csv", "train": "rewards/plr-jumper-GAE/train_rewards-1640360949.407864.csv"},
         "ninja": {"test": "rewards/plr-ninja-GAE/test_rewards-1640090183.7667.csv", "train": "rewards/plr-ninja-GAE/train_rewards-1640090183.7718542.csv"},
         "bigfish": {"test": "rewards/plr-bigfish-GAE/test_rewards-1640075308.8060634.csv", "train": "rewards/plr-bigfish-GAE/train_rewards-1640075308.8102653.csv"}
     },
     "plr-gaem": {
-        "junper": {"test": "rewards/plr-jumper-GAEMag/test_rewards-1640124835.600524.csv", "train": "rewards/plr-jumper-GAEMag/train_rewards-1640124835.6055303.csv"},
+        "jumper": {"test": "rewards/plr-jumper-GAEMag/test_rewards-1640124835.600524.csv", "train": "rewards/plr-jumper-GAEMag/train_rewards-1640124835.6055303.csv"},
         "ninja": {"test": "rewards/plr-ninja-GAEMag/test_rewards-1640101918.664596.csv", "train": "rewards/plr-ninja-GAEMag/train_rewards-1640101918.6686788.csv"},
         "bigfish": {"test": "rewards/plr-bigfish-GAEMag/test_rewards-1640018769.8121777.csv", "train": "rewards/plr-bigfish-GAEMag/train_rewards-1640018769.817248.csv"}
     }
 }
 
-x = np.linspace(1, 22, 22)
+# x = np.linspace(1, 22, 22)
 
 
 def generate_graph(fig, colors, ax, ax_coord, list_of_paths, label_names, title="Results", xlabel="Steps in M",
@@ -53,6 +53,8 @@ def generate_graph(fig, colors, ax, ax_coord, list_of_paths, label_names, title=
         else:
             df = pd.read_csv(path, header=None)
         print(path)
+        x = np.linspace(1, 22, len(df.index))
+        print(len(df.index))
         ax[ax_x, ax_y].plot(x, df, label=label, c=color)
 
         ax[ax_x, ax_y].set_xlabel(xlabel)
@@ -90,9 +92,9 @@ for game, coord in zip(games, coords):
 
 fig.delaxes(axs[1][1])
 
-# handles, labels = axs[1, 1].get_legend_handles_labels()
-# axs[0, 0].legend(bbox_to_anchor=(1.05, 0), loc='lower left', borderaxespad=0.)
-# fig.legend(handles, labels, loc=(0.5, 0), ncol=3)
+handles, labels = axs[1, 1].get_legend_handles_labels()
+axs[0, 0].legend(bbox_to_anchor=(1.05, 0), loc='lower left', borderaxespad=0.)
+fig.legend(handles, labels, loc=(0.5, 0), ncol=3)
 
 fig.tight_layout()
 plt.savefig('rewards.png')
@@ -130,11 +132,12 @@ for label in label_names:
 
     df_test = [d for d in df_test if len(d)>0]
     df_test_mean = pd.concat(df_test, axis=1).mean(axis=1)
-    plt.plot(x, df_test_mean, ":", label=f"{label} test", color=colors[label])
+
+    plt.plot(np.linspace(1, 22, len(df_test_mean.index)), df_test_mean, ":", label=f"{label} test", color=colors[label])
 
     df_train = [d for d in df_train if len(d)>0]
     df_train_mean = pd.concat(df_train, axis=1).mean(axis=1)
-    plt.plot(x, df_train_mean, "--", label=f"{label} train", color=colors[label])
+    plt.plot(np.linspace(1, 22, len(df_train_mean.index)), df_train_mean, "--", label=f"{label} train", color=colors[label])
 
 plt.xlabel("Steps in M")
 plt.ylabel("Rewards")
