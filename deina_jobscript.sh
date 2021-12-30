27b261ff2a6bf1cb852a67fee8fbe1cbd4bacadb
Erik Gylling <gylling.eriuk@gmail.com>
2021-12-30 12:13:53 +0100

    qf

--- deina_jobscript.sh
+++ deina_jobscript.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 bsub < jobscripts/plr-ninja-GAEMag.sh
 bsub < jobscripts/plr-ninja-OneStep.sh
-bsub < jobscripts/plr-ninja-GAE.sh
--- kathrine_jobscript.sh
+++ kathrine_jobscript.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 bsub < jobscripts/plr-2.sh
 bsub < jobscripts/plr-3.sh
+bsub < jobscripts/plr-jumper-GAE.sh
